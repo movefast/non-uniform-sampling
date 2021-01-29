aa62f24ff1cf581e9878e86552f1ef9f4defe30a
movefast <liderek269@gmail.com>
2021-01-29 02:13:56 -0700

    added plotting for maze env

---
+++ maze/plot_maze.py
@@ -0,0 +1,262 @@
+import collections
+import glob
+from collections import Counter
+from datetime import date
+
+import fire
+import numpy as np
+import torch
+from configs import ROOT_DIR
+from matplotlib import pyplot as plt
+from mdp.utils import MinorSymLogLocator
+
+agent_names_in_plot={"NN": "Uniform", "NNP": "Uncertainty", "NNT":"Diversity"}
+env_names_in_plot={'DoorWorldWide3':'GridWorldD3','DoorWorldWide11':'DoorWorldWide13X13D4'}
+titles = {"msbpe":'MSPBE',"ve":'Value Error (VE)', "all_reward_sums":'Sum of Rewards'}
+
+filtered_agent_list = [
+    "Sarsa_NN",
+    "Uniform",
+    "CER",
+    "PER",
+    "PER_V2",
+]
+
+default_color_map = {k:v for k,v in zip(filtered_agent_list, plt.rcParams['axes.prop_cycle'].by_key()['color'])}
+
+y_lims = {"msbpe","ve", "all_reward_sums"}
+y_labels = {"msbpe":"MSPBE","ve":"Value Error (VE)", "all_reward_sums":"Sum of Rewards"}
+stats_metric = {"msbpe":"AUC","ve":"AUC", "all_reward_sums":"Average Rewards"}
+
+# TODO: need to consolidate the definition of env
+env = 'Blocking_Maze'
+
+env_infos = {
+    'Blocking_Maze': {
+        "maze_dim": [6, 8],
+        "start_state": [0, 4],
+        "end_state": [5, 4],
+        # easy
+        # "obstacles": to_list([*zip([2]*7, range(0,7))]),
+        # difficult
+#         "obstacles": to_list([*zip([2]*7, range(1,8))]),
+        "doors": {tuple():[]},
+    },
+#     'Shortcut_Maze': {
+#         "maze_dim": [6, 8],
+#         "start_state": [0, 3],
+#         "end_state": [5, 7],
+#         "obstacles": to_list([*zip([2]*7, range(1,8))]),
+#         "doors": {tuple():[]},
+#     },
+}
+
+num_episodes = 300
+from maze.run_single_job import num_runs
+from maze.write_maze_jobs import params_to_search
+
+
+def plot_metric(metrics, ax, env, algorithm, metric_name):
+    if metric_name == "all_reward_sums":
+        algorithm_means = -np.mean(metrics[metric_name][env][algorithm], axis=0)
+    else:
+        algorithm_means = np.mean(metrics[metric_name][env][algorithm], axis=0)
+    algorithm_stds = np.std(metrics[metric_name][env][algorithm], axis=0)
+    if metric_name == "all_reward_sums":
+        print(algorithm, -np.around(np.mean(algorithm_means),decimals=4), np.around(np.std(np.mean(np.array(metrics[metric_name][env][algorithm]), axis=1))/np.sqrt(num_runs),decimals=4), sep='\t')
+    else:
+        print(algorithm, np.around(np.mean(algorithm_means)*300,decimals=4), np.around(np.std(np.mean(np.array(metrics[metric_name][env][algorithm]), axis=1)*300)/np.sqrt(num_runs),decimals=4), sep='\t')
+
+    ax.plot(algorithm_means, label=env_names_in_plot.get(env,env)+'_'+agent_names_in_plot.get(algorithm, algorithm), alpha=0.5)
+#     ax.set_ylim(0,.005)
+    if metric_name == "msbpe":
+        ax.set_ylim(0,.0015)
+#     ax.set_ylim(-50,-5)
+#     ax.set_ylim(0,.6)
+    ax.fill_between(range(num_episodes), algorithm_means + algorithm_stds/np.sqrt(num_runs), algorithm_means - algorithm_stds/np.sqrt(num_runs), alpha=0.2)
+    ax.legend()
+
+def plot_param_search(metrics):
+    best_agent_names = []
+    for metric_name in ["all_reward_sums"]:
+        fig = plt.figure(figsize=(20,40))
+        fig.subplots_adjust(hspace=0.4, wspace=0.4)
+        print(metric_name)
+        print("agent", stats_metric[metric_name], "SEM", sep='\t')
+
+        for env in env_infos:
+            for i, agent_name in enumerate(filtered_agent_list):
+                ax = fig.add_subplot(5, 2, i+1)
+                agent_names = filter(lambda x: x.startswith(agent_name+"_step_size"),  list(metrics[metric_name][env].keys()))
+                metrics_slice = {agent_name: metrics[metric_name][env][agent_name] for agent_name in agent_names}
+                sorted_agent_name_pairs = sorted([(np.mean(vals), algo) for algo, vals in metrics_slice.items()])
+                for j, (_, algorithm) in enumerate(sorted_agent_name_pairs[:10]):
+                    plot_metric(metrics, ax, env, algorithm, metric_name)
+                    if metric_name == "all_reward_sums" and j == 0:
+                        best_agent_names.append(algorithm)
+        fig.text(0.5, 0.04, 'Episodes', ha='center')
+        fig.text(0.04, 0.5, titles[metric_name], va='center', rotation='vertical')
+
+        fig.suptitle("Top 5 performing model variants per algorithm")
+
+        plt.savefig(ROOT_DIR/f'maze/plots/top_5_{metric_name}.png')
+
+        # plot best learning curves
+        fig, ax = plt.subplots(figsize=(20,10))
+        for algorithm in best_agent_names:
+            plot_metric(metrics, ax, env, algorithm, metric_name)
+        plt.plot([0,299], [-50, -50])
+        plt.ylabel(y_labels[metric_name],rotation=0, labelpad=20)
+        plt.xlabel("Episodes")
+        plt.title("Nonstationary Policy in 100-state RandomWalk (buffer_size=1000)")
+        plt.savefig(ROOT_DIR/f'maze/plots/{metric_name}.png')
+
+def plot_best_learning_curves(metrics):
+    for metric_name in ["all_reward_sums"]:
+        print(metric_name)
+        print("agent", stats_metric[metric_name], "SEM", sep='\t')
+        fig, ax = plt.subplots(figsize=(20,10))
+        for env in env_infos:
+            for i, agent_name in enumerate(filtered_agent_list):
+                agent_names = filter(lambda x: x.startswith(agent_name+"_step_size"),  list(metrics[metric_name][env].keys()))
+                for algorithm in agent_names:
+                    plot_metric(metrics, ax, env, algorithm, metric_name)
+
+            plt.ylabel(y_labels[metric_name],rotation=0, labelpad=20)
+            plt.xlabel("Episodes")
+            # plt.ylim(0,.002)
+            # plt.ylim(-150,-5)
+            # plt.ylim(-800,-5)
+            # plt.ylim(0,.006)
+            # plt.plot([0,500],[-18,-18])
+            # plt.title("Nonstationary Policy in 20-state RandomWalk (buffer_size=1000)")
+            plt.title("Nonstationary Policy in 100-state RandomWalk (buffer_size=1000)")
+            # plt.title("Random Policy in 20-state RandomWalk (buffer_size=1000)")
+
+            # plt.legend()
+            plt.savefig(ROOT_DIR/f'maze/plots/{metric_name}.png')
+
+def get_metric_stats(env, metric_name, algorithm, metrics):
+    if metric_name == "all_reward_sums":
+        algorithm_stats = -np.around(np.mean(metrics[metric_name][env][algorithm]),decimals=4)
+        ste = np.around(np.std(np.mean(np.array(metrics[metric_name][env][algorithm]), axis=1))/np.sqrt(num_runs),decimals=4)
+    else:
+        algorithm_stats = np.around(np.mean(metrics[metric_name][env][algorithm])*300,decimals=4)
+        ste = np.around(np.std(np.mean(np.array(metrics[metric_name][env][algorithm]), axis=1)*300)/np.sqrt(num_runs),decimals=4)
+    return algorithm_stats, ste
+
+import math
+
+
+def plot_parameter_sensitivity(metrics):
+    fig = plt.figure(figsize=(20,10))
+    for metric_name in ["all_reward_sums"]:
+        print(metric_name)
+        all_params = [e for l in [list(v.keys()) for k, v in params_to_search.items() if k in filtered_agent_list] for e in l]
+        params_counter = Counter(all_params)
+        # # 1) plot all unique params
+        # unique_params = set(all_params)
+        # # 2) plot top common params in sorted order
+        # unique_params = [k for k, v in params_counter.most_common(6)]
+        # 3) plot all unique params in sorted order
+        unique_params = sorted(params_counter, key=params_counter.get, reverse=True)
+        unique_params.remove("step_size")
+        for i, param in enumerate(unique_params):
+            ax = fig.add_subplot(3, math.ceil(len(unique_params)/3), i+1)
+            agent_list_for_param =  [agent_type for agent_type in filtered_agent_list if param in params_to_search[agent_type]]
+            # try:
+            #     agent_list_for_param.remove("Sarsa")
+            # except:
+            #     pass
+            for agent_type in agent_list_for_param:
+                x_values = []
+                y_values = []
+                error_bars = []
+                for j, val in enumerate(params_to_search[agent_type][param]):
+                    # print(agent_type, param, val)
+                    agent_names = list(filter(lambda x: x.startswith(agent_type+"_step_size") and (f'{param}_{val}_' in x or x.endswith(f'{param}_{val}')),  list(metrics[metric_name][env].keys())))
+                    # print(agent_names)
+                    metric_stats = [get_metric_stats(env, metric_name, agent_name, metrics) for agent_name in agent_names]
+                    if metric_stats:
+                        lst_of_stats, lst_of_stes = list(zip(*metric_stats))
+                        if param == "step_size":
+                            x_values.append(j)
+                        else:
+                            x_values.append(val)
+                        y_values.append(max(lst_of_stats))
+                        error_bars.append(max(list(zip(lst_of_stats, lst_of_stes)))[1])
+                # print(x_values, y_values)
+                ax.errorbar(x_values, y_values, label=agent_type, yerr=error_bars, capsize=5, elinewidth=1, color=default_color_map[agent_type])#, markeredgewidth=10)
+
+
+            ax.legend()
+            ax.set_title(f'{param}')
+            ax.set_yscale("symlog")
+            # if param == "step_size":
+            #     ax.set_xscale("log")
+    plt.suptitle(f"Sensitivity Analysis in 100-state RandomWalk ({metric_name})")
+    plt.savefig(ROOT_DIR/f'maze/plots/param_study_{metric_name}.png')
+
+
+def plot_step_size_sensitivity(metrics):
+    fig, ax = plt.subplots(figsize=(20,10))
+    for metric_name in ["all_reward_sums"]:
+        print(metric_name, "step_size")
+        param = "step_size"
+        agent_list_for_param =  [agent_type for agent_type in filtered_agent_list if param in params_to_search[agent_type]]
+
+        for agent_type in agent_list_for_param:
+            print(agent_type)
+            x_values = []
+            y_values = []
+            error_bars = []
+            for j, val in enumerate(params_to_search[agent_type][param]):
+                # print(agent_type, param, val)
+                agent_names = list(filter(lambda x: x.startswith(agent_type+"_step_size") and (f'{param}_{val}_' in x or x.endswith(f'{param}_{val}')),  list(metrics[metric_name][env].keys())))
+                # print(agent_names)
+                metric_stats = [get_metric_stats(env, metric_name, agent_name, metrics) for agent_name in agent_names]
+                if metric_stats:
+                    lst_of_stats, lst_of_stes = list(zip(*metric_stats))
+                    if param == "step_size":
+                        x_values.append(j)
+                    else:
+                        x_values.append(val)
+                    y_values.append(max(lst_of_stats))
+                    error_bars.append(max(list(zip(lst_of_stats, lst_of_stes)))[1])
+            print(x_values, y_values)
+            ax.errorbar(x_values, y_values, label=agent_type, yerr=error_bars, capsize=5, elinewidth=1, color=default_color_map[agent_type])#, markeredgewidth=10)
+
+
+            ax.legend()
+            ax.set_title(f'{param}')
+            ax.set_yscale("symlog")
+            y_axis = ax.yaxis
+            # import pdb; pdb.set_trace()
+            y_axis.set_minor_locator(MinorSymLogLocator(1e-1))
+            # if param == "step_size":
+            #     ax.set_xscale("log")
+    plt.suptitle(f"Sensitivity Analysis in 100-state RandomWalk ({metric_name})")
+    plt.savefig(ROOT_DIR/f'maze/plots/param_study_{metric_name}_step_size.png')
+
+
+def plot(plot_type, file_name):
+    today = date.today().strftime("%m_%d")
+    # 1) torch
+    # metrics = torch.load(ROOT_DIR/f'metrics_{today}_{file_name}.torch')
+    # 2)
+    import joblib
+    metrics = joblib.load(ROOT_DIR/f'metrics_{today}_{file_name}.torch')
+    if plot_type == "params":
+        plot_param_search(metrics)
+    elif plot_type == "lc":
+        plot_best_learning_curves(metrics)
+    elif plot_type == "sensitivity":
+        plot_step_size_sensitivity(metrics)
+        plot_parameter_sensitivity(metrics)
+
+
+
+
+
+if __name__ == '__main__':
+    fire.Fire(plot)
