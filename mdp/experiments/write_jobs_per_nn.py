e94221b92be1bc464639fe80f78e1bd7581d8903
movefast <liderek269@gmail.com>
2021-01-31 14:07:03 -0700

    added experiment 2 configs for mdp env

--- mdp/experiments/write_jobs_per_nn.py
+++ mdp/experiments/write_jobs_per_nn.py
@@ -7,7 +7,7 @@ import pandas as pd
 from configs import ROOT_DIR
 from fastprogress.fastprogress import master_bar, progress_bar
 
-MAX_EVALS=200
+MAX_EVALS=150
 count = 0
 
 
@@ -24,7 +24,7 @@ def create_job(agent_type, hyper_params):
 
 def random_search(agent_type, param_grid, mb=master_bar(range(1)), max_evals=MAX_EVALS):#, num_runs=10):
     """Random search for hyperparameter optimization"""
-    
+
     # Keep searching until reach max evaluations
     for j in mb:
         for i in progress_bar(range(max_evals),parent=mb):
@@ -39,22 +39,24 @@ def random_search(agent_type, param_grid, mb=master_bar(range(1)), max_evals=MAX
 def grid_search(agent_type, param_grid):#, num_runs=10):
     """grid search for hyperparameter optimization"""
     param_keys, values = zip(*param_grid.items())
-    
+
     param_combos = [dict(zip(param_keys, combo)) for combo in product(*values)]
-    
+
     mb = master_bar(param_combos)
-    
+
     for i, hyper_params in enumerate(mb):
         print(hyper_params)
         create_job(agent_type, hyper_params)
 
 agents = {
     # "Uniform": None,
+    "PER": None,
     "PER_V2": None,
-    "GEO_min_weight": None,
-    "GEO_min_weight_adaptive_decay": None,
-    "PER_GEO": None,
-    "PER_WO_Recency_Bias_GEO": None,
+    # "GEO_min_weight": None,
+    # "GEO_min_weight_adaptive_decay": None,
+    # "GEO_sim": None,
+    # "PER_GEO": None,
+    # "PER_WO_Recency_Bias_GEO": None,
 }
 # param_grid = dict(
 #     step_size=[1e-2,1e-3,3e-4],
@@ -65,45 +67,70 @@ def get_lr(b=1e-2, a=2, n=5):
 
 
 params_to_search = {
+    # 1)
+    # "PER_V2": {
+    #     "step_size": get_lr(n=8),
+    #     "per_alpha": get_lr(b=1,n=4),
+    #     "buffer_beta":get_lr(b=1,n=4),
+    # },
+    # "GEO_min_weight": {
+    #     "step_size": get_lr(n=8),
+    #     "buffer_beta":get_lr(b=1,n=4),
+    #     "geo_alpha": [0],
+    #     "tau": get_lr(b=1,n=8),
+    #     "min_weight": get_lr(b=0.2, n=5),
+    # },
+    # "GEO_min_weight_adaptive_decay": {
+    #     "step_size": get_lr(n=8),
+    #     "buffer_beta":get_lr(b=1,n=4),
+    #     "geo_alpha": get_lr(b=1,n=4),
+    #     "tau": get_lr(b=1,n=8),
+    #     "min_weight": get_lr(b=0.2, n=5),
+    # },
+    # "GEO_sim": {
+    #     "step_size": [0.01, 0.005, 0.0025, 0.00125, 0.000625, 0.00015625, 7.8125e-05],
+    #     "buffer_beta":get_lr(b=1,n=4),
+    #     "geo_alpha": get_lr(b=1,n=4),
+    #     "tau": get_lr(b=1,n=8),
+    #     "min_weight": get_lr(b=0.2, n=5),
+    #     "sim_mode": [1, 2],
+    # },
+    # "PER_GEO": {
+    #     "step_size": [0.0003125],
+    #     "per_alpha": get_lr(b=1,n=4),
+    #     "buffer_beta":get_lr(b=1,n=4),
+    #     "geo_alpha": get_lr(b=1,n=4),
+    #     "tau": get_lr(b=1,n=8),
+    #     "recency_bias": [True],
+    #     "min_weight": [0],
+    #     "lam": [0.5, 1, 2, 3, 5],
+    # },
+    # "PER_WO_Recency_Bias_GEO": {
+    #     "step_size": [0.0003125],
+    #     "per_alpha": get_lr(b=1,n=4),
+    #     "buffer_beta":get_lr(b=1,n=4),
+    #     "geo_alpha": get_lr(b=1,n=4),
+    #     "tau": get_lr(b=1,n=8),
+    #     "recency_bias": [False],
+    #     "min_weight": [0],
+    #     "lam": [0.5, 1, 2, 3, 5],
+    # },
+    # 2)
+    "PER": {
+        "step_size": get_lr(n=8),
+        "per_alpha": get_lr(b=1,n=4),
+        "buffer_beta":get_lr(b=1,n=4),
+        "min_weight": get_lr(b=.1,n=5)+[0],
+    },
     "PER_V2": {
         "step_size": get_lr(n=8),
-        "per_alpha": get_lr(b=1,n=4), 
+        "per_alpha": get_lr(b=1,n=4),
+        "geo_alpha": get_lr(b=1,n=4),
         "buffer_beta":get_lr(b=1,n=4),
+        "tau": get_lr(b=1,n=10)+[0],
+        "min_weight": get_lr(b=.1,n=5)+[0],
     },
-    "GEO_min_weight": {
-        "step_size": get_lr(n=8),
-        "buffer_beta":get_lr(b=1,n=4),
-        "geo_alpha": [0], 
-        "tau": get_lr(b=1,n=8),
-        "min_weight": get_lr(b=0.2, n=5),
-    },
-    "GEO_min_weight_adaptive_decay": {
-        "step_size": get_lr(n=8),
-        "buffer_beta":get_lr(b=1,n=4),
-        "geo_alpha": get_lr(b=1,n=4), 
-        "tau": get_lr(b=1,n=8),
-        "min_weight": get_lr(b=0.2, n=5),
-    }, 
-    "PER_GEO": {
-        "step_size": get_lr(n=8),
-        "per_alpha": get_lr(b=1,n=4), 
-        "buffer_beta":get_lr(b=1,n=4),
-        "geo_alpha": get_lr(b=1,n=4), 
-        "tau": get_lr(b=1,n=8),
-        "recency_bias": [True],
-        "min_weight": get_lr(b=0.2, n=5),
-        "lam": [0.5, 1, 2, 3, 5],
-    },  
-    "PER_WO_Recency_Bias_GEO": {
-        "step_size": get_lr(n=8),
-        "per_alpha": get_lr(b=1,n=4), 
-        "buffer_beta":get_lr(b=1,n=4),
-        "geo_alpha": get_lr(b=1,n=4), 
-        "tau": get_lr(b=1,n=8),
-        "recency_bias": [False],
-        "min_weight": get_lr(b=0.2, n=5),
-        "lam": [0.5, 1, 2, 3, 5],
-    },
+
 }
 
 if __name__ == "__main__":
@@ -112,10 +139,12 @@ if __name__ == "__main__":
         if agent_type == 'Meta_PER':
             random_search(agent_type, params_to_search[agent_type], max_evals=400)
         # elif agent_type in ('PER', 'GEO', "Meta_CER"):
-        elif agent_type in ("GEO_min_weight", "GEO_min_weight_adaptive_decay", "PER_GEO", "PER_WO_Recency_Bias_GEO"):
+        # elif agent_type in ("GEO_min_weight", "GEO_min_weight_adaptive_decay", "PER_GEO", "PER_WO_Recency_Bias_GEO"):
+        elif agent_type in ('PER', 'PER_V2'):
             random_search(agent_type, params_to_search[agent_type])
+        elif agent_type == "GEO_sim":
+            random_search(agent_type, params_to_search[agent_type], max_evals=200)
         else:
             grid_search(agent_type, params_to_search[agent_type])
 
         print('Jobs: sbatch --array={}-{} ./mdp/jobs/run_cpu.sh'.format(0, count-1))
-        
--- mdp/run_single_job.py
+++ mdp/run_single_job.py
@@ -39,7 +39,7 @@ gamma = .99
 num_states = 100
 num_runs = 25
 num_episodes = 300
-exp_decay_explor = True 
+exp_decay_explor = True
 
 
 nb_dir = os.path.split(os.getcwd())[0]
@@ -47,7 +47,7 @@ if nb_dir not in sys.path:
     sys.path.append(nb_dir)
 
 cur_dir = ROOT_DIR/"mdp"
-    
+
 # random policy
 def get_pred_error(agent, epsilon_greedy=False):
     # TODO: change 5 to num of state in the env
@@ -71,12 +71,12 @@ def get_pred_error(agent, epsilon_greedy=False):
         policy.scatter_(1, action_values.max(1)[1].unsqueeze(-1), (1 - agent.epsilon))
         policy += agent.epsilon / 2
         policy = policies.fromStateArray(policy.cpu().detach().numpy())
-        
-    
+
+
     P = rw_env.buildTransitionMatrix(policy)
     R =rw_env.buildAverageReward(policy)
     d = rw_env.getSteadyStateDist(policy)
-    
+
     AbC = partiallyApplyMSPBE(X, P, R, d, gamma)
     theta = action_values @ torch.tensor([.5, .5])
     if epsilon_greedy:
@@ -90,10 +90,10 @@ def get_pred_error(agent, epsilon_greedy=False):
 def run_episode(env, agent, state_visits=None, keep_history=False):
     is_terminal = False
     step_count = 0
-    
+
     obs = env.env_start(keep_history=keep_history)
     action = agent.agent_start(obs)
-    
+
     if state_visits is not None:
         state_visits[obs[0]] += 1
 
@@ -112,7 +112,7 @@ def run_episode(env, agent, state_visits=None, keep_history=False):
 
         if state_visits is not None:
             state_visits[state[0]] += 1
-    
+
     targets = np.flip([gamma**n for n in range(1,num_states+1)]).copy()# * (1 - agent.epsilon / 2)
     if type(agent) is SarsaAgent or type(agent) is SarsaLAgent:
         predictions = agent.q.max(axis=1)[1:-1] * (1 - agent.epsilon) \
@@ -135,7 +135,7 @@ def run_episode(env, agent, state_visits=None, keep_history=False):
         return step_count, msbpe, ve, history
     else:
         return step_count, msbpe, ve
-    
+
 
 envs = {
     'Grid-World': MazeEnvironment,
@@ -144,8 +144,8 @@ envs = {
 
 env_infos = {
     'MDP': {
-        "maze_dim": [1, num_states+2], 
-        "start_state": [0, num_states//2+1], 
+        "maze_dim": [1, num_states+2],
+        "start_state": [0, num_states//2+1],
         "end_state": [0, num_states+1],
         "obstacles":[],
         "doors": {tuple():[]},
@@ -155,7 +155,7 @@ env_infos = {
 
 agents = {
     "Uniform": NNAgent,
-    "PER": PERAgent,
+    "PER": PERAgentV2,
     "PER_wo_Recency_Bias": PERAgent,
     "PER_V2": PERAgentV2,
     "GEO_min_weight": PERAgentV2,
@@ -182,11 +182,11 @@ agent_infos = {
     "Sarsa_lambda": {"step_size": .1, "buffer_size": 100, "batch_size": 1, "lambda":.9},
     "Uniform": {"step_size": 1e-2, "buffer_size": 1000, "batch_size": 10, "num_meta_update":1},
     "CER": {"step_size": 1e-2, "buffer_size": 1000, "batch_size": 10, "k":1, "num_meta_update":1},
-    "PER": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "buffer_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False, "num_meta_update":1},
-    "PER_V2": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "per_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False, "num_meta_update":1, "weighting_strat":1},
-    "GEO_min_weight": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "geo_alpha": 0, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False,"tau": 0.01, "min_weight":.01, "num_meta_update":1, "weighting_strat":2},
-    "GEO_min_weight_adaptive_decay": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "geo_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False,"tau": 0.01, "min_weight":.01, "num_meta_update":1, "weighting_strat":2},
-    "GEO_sim": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "geo_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False,"tau": 0.01, "min_weight":.01, "num_meta_update":1, "weighting_strat":2},
+    "PER": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "buffer_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False, "num_meta_update":1, "decay_by_uncertainty": False},
+    "PER_V2": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "per_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False, "num_meta_update":1, "weighting_strat":1, "decay_by_uncertainty": False},
+    "GEO_min_weight": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "geo_alpha": 0, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False,"tau": 0.01, "min_weight":.01, "num_meta_update":1, "weighting_strat":2, "decay_by_uncertainty": False},
+    "GEO_min_weight_adaptive_decay": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "geo_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False,"tau": 0.01, "min_weight":.01, "num_meta_update":1, "weighting_strat":2, "decay_by_uncertainty": False},
+    "GEO_sim": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "geo_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False,"tau": 0.01, "min_weight":.01, "num_meta_update":1, "weighting_strat":2, "decay_by_uncertainty": False},
     "PER_GEO": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "geo_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": False,"tau": 0.01, "lam": 2, "min_weight":.01, "num_meta_update":1, "weighting_strat":3},
     "PER_WO_Recency_Bias_GEO": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "geo_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": False, "grad_norm": False,"tau": 0.01, "lam": 2, "min_weight":.01, "num_meta_update":1, "weighting_strat":3},
     "GNorm": {"step_size": 3e-3, "buffer_size": 1000, "batch_size": 10, "correction":True, "buffer_alpha":0.6, "buffer_beta":0.4, "beta_increment":1e-4, "recency_bias": True, "grad_norm": True, "num_meta_update":1},
@@ -208,15 +208,15 @@ metrics = {"msbpe":{},"ve":{}, "all_reward_sums": {}, "hyper_params": {}, "state
 
 def objective(agent_type, hyper_params, num_runs=num_runs):
     start = time.time()
-    
+
     Environment = envs['Grid-World']
-    
+
     all_state_visits = {} # Contains state visit counts during the last 10 episodes
     all_history = {}
 
     mb = master_bar(env_infos.items())
     algorithm = agent_type + '_' + '_'.join([f'{k}_{v}' for k, v in hyper_params.items()])
-    
+
     for env_name, env_info in mb:
         print(env_name)
         if env_name not in all_state_visits:
@@ -225,14 +225,14 @@ def objective(agent_type, hyper_params, num_runs=num_runs):
             if env_name not in metrics[metric]:
                 metrics[metric][env_name] = {}
                 metrics[metric][env_name].setdefault(algorithm, [])
-            
+
         all_state_visits[env_name][algorithm] = []
         for run in progress_bar(range(num_runs), parent=mb):
             agent = agents[agent_type]()
             env = Environment()
             env.env_init(env_info)
 #             print(env_info)
-            agent_info = {"num_actions": 2, "num_states": env.cols * env.rows, "epsilon": 1, "step_size": 0.1, "discount": gamma} 
+            agent_info = {"num_actions": 2, "num_states": env.cols * env.rows, "epsilon": 1, "step_size": 0.1, "discount": gamma}
             agent_info["seed"] = run
             agent_info.update(agent_infos[agent_type])
             agent_info.update(hyper_params)
@@ -256,11 +256,11 @@ def objective(agent_type, hyper_params, num_runs=num_runs):
                 agent.epsilon = epsilon
                 if episode < num_episodes - 10:
                     episode_start = time.time()
-                    sum_of_rewards, msbpe, ve = run_episode(env, agent) 
+                    sum_of_rewards, msbpe, ve = run_episode(env, agent)
                     episode_end = time.time()
                     if episode_end - episode_start > 15:
                         return False
-                else: 
+                else:
 #                   Runs an episode while keeping track of visited states and history
                     sum_of_rewards, msbpe, ve, history = run_episode(env, agent, state_visits, keep_history=True)
                     all_history.setdefault(env_name, {}).setdefault(algorithm, []).append(history)
